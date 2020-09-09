f5fb0cd76cff61b68a29d1b4cf745da086a23059
Diego Orlando <diego.orlando@accendoit.com>
2020-09-09 19:21:21 -0300

    First commit | Player with movement and animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movSpeed = 5f;
+    private float screenMarginLimitX;
+    private float screenMarginLimitY;
+    public GameObject projectilePrefab;
+    public Transform projectileStartPointLeft;
+    public Transform projectileStartPointRight;
+    public Transform projectileStartPointUp;
+    public Transform projectileStartPointDown;
+    public Animator animator;
+    private static Direction direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenMarginLimitX = Camera.main.orthographicSize * 0.9f * Camera.main.aspect;
+        screenMarginLimitY = Camera.main.orthographicSize * 0.85f;
+        direction = Direction.Up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimiento horizontal
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Direction.Left;
+            animator.SetInteger("KeyPressed", Convert.ToInt32(direction));            
+            var x = Math.Max(transform.position.x - movSpeed * Time.deltaTime, -screenMarginLimitX);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            UnityEngine.Debug.Log("Left = " + KeyCode.LeftArrow);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Direction.Right;
+            animator.SetInteger("KeyPressed", Convert.ToInt32(direction));
+            var x = Math.Min(transform.position.x + movSpeed * Time.deltaTime, screenMarginLimitX);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            UnityEngine.Debug.Log("Right = " + KeyCode.RightArrow);
+        }
+
+        //Movimiento vertical
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = Direction.Up;
+            animator.SetInteger("KeyPressed", Convert.ToInt32(direction));
+            var y = Math.Min(transform.position.y + movSpeed * Time.deltaTime, screenMarginLimitY);
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            UnityEngine.Debug.Log("Up = " + KeyCode.UpArrow);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = Direction.Down;
+            animator.SetInteger("KeyPressed", Convert.ToInt32(direction));
+            var y = Math.Max(transform.position.y - movSpeed * Time.deltaTime, -screenMarginLimitY);
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            UnityEngine.Debug.Log("Down = " + KeyCode.DownArrow);
+        }
+
+        //Disparos
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot(direction);
+        }
+    }
+
+    private void Shoot(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Left:
+                GameObject.Instantiate(projectilePrefab, projectileStartPointLeft.position, Quaternion.identity);
+                break;
+            case Direction.Right:
+                GameObject.Instantiate(projectilePrefab, projectileStartPointRight.position, Quaternion.identity);
+                break;
+            case Direction.Up:
+                GameObject.Instantiate(projectilePrefab, projectileStartPointUp.position, Quaternion.identity);
+                break;
+            case Direction.Down:
+                GameObject.Instantiate(projectilePrefab, projectileStartPointDown.position, Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    enum Direction
+    {
+        Left = 0,
+        Right = 1,
+        Up = 2,
+        Down = 3
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float movSpeed = 10f;
+    private bool movesLeft = false;
+    private bool movesRight = false;
+    private bool movesUp = false;
+    private bool movesDown = false;
+    private float screenMarginLimitX;
+    private float screenMarginLimitY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            movesLeft = true;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            movesRight = true;
+        if (Input.GetKey(KeyCode.UpArrow))
+            movesUp = true;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            movesDown = true;
+
+        screenMarginLimitX = Camera.main.orthographicSize * 0.9f * Camera.main.aspect;
+        screenMarginLimitY = Camera.main.orthographicSize * 0.85f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = transform.position.x;
+        var y = transform.position.y;
+        if (movesLeft)
+            x -= movSpeed * Time.deltaTime;
+        else if (movesRight)
+            x += movSpeed * Time.deltaTime;
+        if (movesUp)
+            y += movSpeed * Time.deltaTime;
+        else if (movesDown)
+            y -= movSpeed * Time.deltaTime;
+
+        if(!movesLeft && !movesRight && !movesUp && !movesDown)
+            y += movSpeed * Time.deltaTime;
+
+        if (x > -screenMarginLimitX && x < screenMarginLimitX && y > -screenMarginLimitY && y < screenMarginLimitY)
+            transform.position = new Vector3(x, y, transform.position.z);
+        else
+            Destroy();
+    }
+
+    private void Destroy()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
